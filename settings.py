79b5989f756259fd123ec7677ddcf983a08e3757
Pedro Rodrigues <prodrigues1990@gmail.com>
2019-04-13 01:58:43 +0100

    pass mongo port as int

--- settings.py
+++ settings.py
@@ -112,7 +112,7 @@ DOMAIN = {
 # We want to seamlessy run our API both locally and on Heroku. If running on
 # Heroku, sensible DB connection settings are stored in environment variables.
 MONGO_HOST = os.environ.get('MONGO_HOST', 'localhost')
-MONGO_PORT = os.environ.get('MONGO_PORT', 27017)
+MONGO_PORT = int(os.environ.get('MONGO_PORT', 27017))
 MONGO_USERNAME = os.environ.get('MONGO_USERNAME', '')
 MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD', '')
 MONGO_DBNAME = os.environ.get('MONGO_DBNAME', 'vatsim-status-proxy')
