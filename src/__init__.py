f6a38373a7d97013b14f391fdf08920a686ecdae
Pedro Rodrigues <prodrigues1990@gmail.com>
2019-04-16 23:43:22 +0100

    add pre get logic to (re)populate database

--- src/__init__.py
+++ src/__init__.py
@@ -16,17 +16,62 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with VATSIM Status Proxy.  If not, see <http://www.gnu.org/licenses/>.
 """
+from datetime import datetime, timedelta
 from eve import Eve
 from . import vatsim_data, icao_data
+from .vatsim import VatsimStatus
 
 app = Eve(__name__)
 
 def pre_get_callback(resource, request, lookup):
-    if resource not in ['clients', 'prefiles', 'servers']:
+    if resource not in ['voice_servers', 'clients', 'servers', 'prefiles']:
         return
 
-    if app.debug or vatsim_data.should_update():
-        vatsim_data.update()
+    db = app.data.driver.db['dataversion']
+    dataversion = db.find_one()
+    now = datetime.utcnow()
+    if dataversion:
+        _last_update = dataversion['_updated'].replace(tzinfo=None)
+    else:
+        _last_update = now - timedelta(seconds=60)
+    seconds_since_update = (now - _last_update).total_seconds()
+
+    if seconds_since_update < 60 and not app.debug:
+        return
+
+    if dataversion:
+        dataversion['_updated'] = now
+        db.save(dataversion)
+    else:
+        db.insert_one({'_created': now, '_updated': now})
+
+    status = VatsimStatus.from_url()
+    def save(existing, new):
+        new['_updated'] = now
+        if existing:
+            existing.update(new)
+            db.save(existing)
+        else:
+            new['_created'] = now
+            db.insert_one(new)
+
+    db = app.data.driver.db['voice_servers']
+    for item in status.voice_servers:
+        existing = db.find_one({'hostname_or_IP': item['hostname_or_IP']})
+        save(existing, item)
+    db = app.data.driver.db['clients']
+    for item in status.clients:
+        existing = db.find_one(
+            {'callsign': item['callsign'], 'cid': item['cid'], 'clienttype': item['clienttype']})
+        save(existing, item)
+    db = app.data.driver.db['servers']
+    for item in status.servers:
+        existing = db.find_one({'hostname_or_IP': item['hostname_or_IP']})
+        save(existing, item)
+    db = app.data.driver.db['prefile']
+    for item in status.prefile:
+        existing = db.find_one({'callsign': item['callsign'], 'cid': item['cid']})
+        save(existing, item)
 
 app.on_pre_GET += pre_get_callback
 
