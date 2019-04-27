3e4e6398fd50dc9d3e7deaa37d3e418579c09a5f
Tiago Vicente <tmavicente@gmail.com>
2019-04-27 03:44:32 +0100

    read vatspy.dat

---
+++ src/vatspy_dat.py
@@ -0,0 +1,97 @@
+"""
+VATSIM Status Proxy
+Copyright (C) 2017 - 2019  Pedro Rodrigues <prodrigues1990@gmail.com>
+
+This file is part of VATSIM Status Proxy.
+
+VATSIM Status Proxy is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 2 of the License.
+
+VATSIM Status Proxy is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with VATSIM Status Proxy.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import sys
+
+_current_module = sys.modules[__name__] # pylint: disable=C0103
+
+# pylint: disable=R0903, R0902
+class VatspyDat():
+    """Dataclass holding the information provided by VATspy.dat."""
+    def __init__(self, file):
+        self.countries = list()
+        self.airports = list()
+        self.firs = list()
+        self.uirs = list()
+        self.idl = list()
+
+        for section in file.replace('\n\n\n', '\n\n').split('\n\n'):
+            section = section.split('\n')
+            section_name = section[0][1:len(section[0])-1].lower()
+            section.pop(0)
+
+            for line in section:
+                if line.startswith(';'):
+                    continue
+                _section = f'_split_{section_name}'
+                try:
+                    line = vars(_current_module)[_section](line)
+                    getattr(self, section_name).append(line)
+                except AttributeError:
+                    pass
+
+    @staticmethod
+    def from_file(data='VATSpy.dat'):
+        """
+        Tests
+        """
+        with open(data, 'r') as file:
+            return VatspyDat(file.read())
+
+def _split_to_dict(keys, line, *, separator='|'):
+    values = line.split(separator)[:len(keys)]
+    assert len(keys) >= len(values), f'{len(keys)} != {len(values)} for {line}'
+    return {key: value for key, value in zip(keys, values)}
+
+def _split_countries(line):
+    keys = (
+        'country',
+        'code',
+        'atc_prefix')
+    return _split_to_dict(keys, line)
+
+def _split_airports(line):
+    keys = (
+        'ICAO',
+        'name',
+        'lat',
+        'long',
+        'atc_short_prefix',
+        'FIR',)
+    return _split_to_dict(keys, line)
+
+def _split_firs(line):
+    keys = (
+        'ICAO',
+        'name',
+        'atc_prefix')
+    return _split_to_dict(keys, line)
+
+def _split_uirs(line):
+    keys = (
+        'ICAO',
+        'name',
+        'FIRs')
+    return _split_to_dict(keys, line)
+
+def _split_idl(line):
+    keys = (
+        'lat',
+        'lng')
+    return _split_to_dict(keys, line)
---
+++ tests/test_vatspy.py
@@ -0,0 +1,37 @@
+"""
+VATSIM Status Proxy
+Copyright (C) 2017 - 2019  Pedro Rodrigues <prodrigues1990@gmail.com>
+
+This file is part of VATSIM Status Proxy.
+
+VATSIM Status Proxy is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 2 of the License.
+
+VATSIM Status Proxy is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with VATSIM Status Proxy.  If not, see <http://www.gnu.org/licenses/>.
+"""
+import unittest
+
+from src.vatspy_dat import VatspyDat
+
+class VatsimTest(unittest.TestCase):
+    """Tests for Vatspy dataclasses."""
+    def test_dat(self):
+        """Test against a sample version of the VATspy.dat information."""
+        with open('VATSpy.dat', 'r') as file:
+            file = file.read()
+        status = VatspyDat(file)
+
+        for item in [*status.countries, *status.airports, *status.firs, *status.uirs, *status.idl]:
+            self.assertIs(type(item), dict)
+        self.assertTrue(len(status.countries) > 0)
+        self.assertTrue(len(status.airports) > 0)
+        self.assertTrue(len(status.firs) > 0)
+        self.assertTrue(len(status.uirs) > 0)
+        self.assertTrue(len(status.idl) > 0)
