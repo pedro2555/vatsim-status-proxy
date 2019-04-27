6b5d649fce5fdce421011e11681eb535cd35d1a7
Tiago Vicente <tmavicente@gmail.com>
2019-04-27 06:01:09 +0100

    add func to ger geojson boundaries

--- src/fir_boundaries_dat.py
+++ src/fir_boundaries_dat.py
@@ -19,6 +19,7 @@ along with VATSIM Status Proxy.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
 from collections import defaultdict
+import geojson
 
 _current_module = sys.modules[__name__] # pylint: disable=C0103
 
@@ -33,14 +34,32 @@ class FIRBoundaries():
             if line[1].isalpha():
                 icao = line.split('|')[0]
                 _section = f'_split_infos'
+                try:
+                    line = vars(_current_module)[_section](line)
+                    getattr(self, 'boundaries')[icao] = defaultdict(list)
+                    getattr(self, 'boundaries')[icao]['infos'] = line
+                except AttributeError:
+                    pass
             else:
                 _section = f'_split_coords'
+                try:
+                    line = vars(_current_module)[_section](line)
+                    getattr(self, 'boundaries')[icao]['boundaries'].append(line)
+                except AttributeError:
+                    pass
 
-            try:
-                line = vars(_current_module)[_section](line[1:])
-                getattr(self, 'boundaries')[icao].append(line)
-            except AttributeError:
-                pass
+    def get_fir_polygons(self, icao):
+        """Returns a valid geojson boundaries.
+
+        Args:
+            icao (str): A valid icao.
+
+        Returns:
+            get_fir_polygons: object with status file information."""
+        self.boundaries[icao]['boundaries'].append(self.boundaries[icao]['boundaries'][0])
+        polygons = geojson.Polygon(
+            [[[x['lng'], x['lat']] for x in self.boundaries[icao]['boundaries']]])
+        return polygons
 
     @staticmethod
     def from_file(data='FIRBoundaries.dat'):
@@ -61,16 +80,41 @@ def _split_to_dict(keys, line, *, separator='|'):
 
 def _split_infos(line):
     keys = (
+        'icao',
         'unknow',
         'unknow1',
         'coords_count',
-        'limit1',
-        'limit2',
-        'name_position')
-    return _split_to_dict(keys, line)
+        'limit1_lat',
+        'limit1_lng',
+        'limit2_lat',
+        'limit2_lng',
+        'name_position_lat',
+        'name_position_lng')
+    result = _split_to_dict(keys, line)
+    result['limit1'] = geojson.Point((float(result['limit1_lng']), float(result['limit1_lat'])))
+    del result['limit1_lng'], result['limit1_lat']
+
+    result['limit2'] = geojson.Point((float(result['limit2_lng']), float(result['limit2_lat'])))
+    del result['limit2_lng'], result['limit2_lat']
+
+    result['name_position'] = geojson.Point(
+        (float(result['name_position_lng']), float(result['name_position_lat'])))
+    del result['name_position_lng'], result['name_position_lat']
+
+    return result
 
 def _split_coords(line):
     keys = (
         'lat',
         'lng')
-    return _split_to_dict(keys, line)
+    result = _split_to_dict(keys, line)
+    types = {
+        'lat': float,
+        'lng': float
+    }
+    for key, func in types.items():
+        value = result[key].strip()
+
+        value = value if value != '' else 0
+        result[key] = func(value)
+    return result
