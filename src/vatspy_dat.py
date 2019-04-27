8c12f12d1ccee9c24e6a423dc6ebf14976e62dee
Tiago Vicente <tmavicente@gmail.com>
2019-04-27 04:29:50 +0100

    fix volta ao bilhar

--- src/vatspy_dat.py
+++ src/vatspy_dat.py
@@ -34,7 +34,7 @@ class VatspyDat():
         for line in lines:
             line.strip()
             if line.startswith('['):
-                section_name = lines[lines.index(line)][1:len(lines[lines.index(line)])-2].lower()
+                section_name = line[1:len(line)-2].lower()
                 lines.pop(lines.index(line))
 
             if line.startswith(';'):
